7071d1da96acc2d32612fbd71c9f4dd87486d059
Jared Harper <jared.harper@gmail.com>
2015-01-16 12:22:29 -0600

    initial commit

---
+++ src/com/none/svmwrapper/Main.java
@@ -0,0 +1,27 @@
+package com.none.svmwrapper;
+
+
+public class Main
+{
+	
+	public void scale()
+	{
+		
+	}
+	
+	public void train()
+	{
+		
+	}
+	
+	public void test()
+	{
+		
+	}
+	
+	public void apply()
+	{
+		
+	}
+
+}
