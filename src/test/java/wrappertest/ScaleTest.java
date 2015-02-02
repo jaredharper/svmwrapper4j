72cf84099a18c9ed2b22820725b6de764a7da643
Jared Harper <jared.harper@gmail.com>
2015-02-02 11:18:45 -0600

    First draft of unit test for Scale

--- src/test/java/wrappertest/ScaleTest.java
+++ src/test/java/wrappertest/ScaleTest.java
@@ -2,7 +2,17 @@ package wrappertest;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.After;
 import org.junit.Before;
@@ -28,16 +38,69 @@ public class ScaleTest
 	@Test
 	public void test()
 	{
-		
-		// Read input data
-		
-		// Load input data into dv		
-		ArrayList<DataElement> dv = new ArrayList<>();
-		
-		// Perform scale operation
-		Scale.scale(dv);	
-		
-		// Read scaled data
+
+		// Read input data		
+		try (BufferedReader r = new BufferedReader(new FileReader(new File("src/test/data/sample.txt"))))
+		{
+			ArrayList<DataElement> elements = new ArrayList<>();
+			for (String line = r.readLine(); line != null; line = r.readLine())
+			{
+				DataElement de = new DataElement();
+				
+				String[] components = line.split(" ");
+				
+				// Get the first token in the line, which is a class label
+				int label = Integer.parseInt(components[0]);
+				de.setClassLabel(label);
+				
+				// Get the index:value pairs
+				// This does NOT support sparse Arrays
+				// Mostly because I don't use sparse arrays
+				// XXX TODO FIXME support sparse arrays
+				ArrayList<Double> data = new ArrayList<>();
+				String[] t = Arrays.copyOfRange(components, 1, components.length);
+				//for (String pair : Arrays.copyOfRange(components, 1, components.length))
+				for (int i = 0; i < t.length; i++)
+				{
+					String pair = t[i];
+					Double d = 0.0;
+					try
+					{
+						d = Double.parseDouble(pair.split(":")[1]);
+					}
+					catch (Exception e)
+					{
+						Logger.getAnonymousLogger().log(Level.INFO,pair.toString());
+						fail();
+					}
+					data.add(d);
+				}
+				Double[] d = new Double[data.size()];
+				data.toArray(d);
+				de.setData(d);
+				
+				elements.add(de);
+			}
+			
+			// Perform scale operation
+			Scale.scale(elements);	
+			
+			// Read scaled data
+			for (DataElement e : elements)
+			{
+				Double[] d = e.getData();
+				for (Double value : d)
+				{
+					if (value < -1.0 || value > 1.0)
+						fail();
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Logger.getAnonymousLogger().log(Level.SEVERE,ex.getMessage());
+			fail();
+		}
 	}
 
 }
